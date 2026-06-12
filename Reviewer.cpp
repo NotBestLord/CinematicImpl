ed17ca3bcb5d7ca0ee67796658055999cc693640
NotLord <amir01zx@gmail.com>
2026-06-12 10:31:19 +0300

    Create Reviewer.cpp

---
+++ Reviewer.cpp
@@ -0,0 +1,30 @@
+#include "Reviewer.h"
+#include <cstring>
+
+Reviewer::Reviewer(const char* name, int id, const char* publicationName, int visitCount = 0) :
+    Guest(name, id, visitCount)
+{
+    this->publicationName = strdup(publicationName);
+}
+Reviewer::Reviewer(const Reviewer& other) : Guest(other), publicationName(nullptr)
+{
+    *this = other;
+}
+Reviewer& Reviewer::operator=(const Reviewer& other)
+{
+    if (this != &other)
+    {
+        delete[]this->publicationName;
+        this->publicationName = strdup(other.publicationName);
+    }
+    return *this;
+}
+Reviewer::~Reviewer()
+{
+    delete []this->publicationName;
+}
+
+void Reviewer::toOs(std::ostream& os) const
+{
+    os << ", " << this->publicationName << ", " << this->getVisitCount();
+}
